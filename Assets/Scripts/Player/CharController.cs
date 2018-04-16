a90edae823083730977d925a675278bee335d163
TKlintworth <frostflag@gmail.com>
2018-04-15 23:17:12 -0400

    Added Character Rotate to Mouse Cursor
    It's there, but not fully functional as planned. Added to CharController script

---
+++ Assets/Scripts/Player/CharController.cs
@@ -0,0 +1,140 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharController : MonoBehaviour {
+
+    [SerializeField]
+    public float moveSpeed = 4f;
+
+    public GameObject player;
+    
+    Vector3 forward, right;
+
+    int floorMask;
+    Rigidbody playerRigidbody;
+    float camRayLength = 1000f;
+
+    void Awake()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+        floorMask = LayerMask.GetMask("Floor");
+        Debug.Log("Floormask: " + floorMask);
+        Debug.Log("playerRigidBody: " + playerRigidbody);
+    }
+
+    void FixedUpdate()
+    {
+        Turning();
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        forward = Camera.main.transform.forward;
+        forward.y = 0;
+        forward = Vector3.Normalize(forward);
+        //Debug.Log("Forward: " + forward);
+   
+        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+        //Debug.Log("right: " + right);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Turning();
+        if (forward != Vector3.zero)
+        {
+            if (Input.anyKey)
+            {
+                Move();
+            }
+        }
+       
+	}
+
+    void Move()
+    {
+        //let burnCalories know that we're moving now
+        //player.GetComponent<burnCalories>().isMoving = true;
+        //Left Shift send to Sprint to hopefully increase speed
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Sprint();
+            player.GetComponent<burnCalories>().modCalorieBurn();
+        }
+        Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey");
+        Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("VerticalKey");
+
+        Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+
+        //Debug.Log("heading: " + heading);
+        //Debug.Log("Right movement: " + rightMovement);
+        //Debug.Log("up movement: " + upMovement);
+
+        //Check if heading vector is zero, if not, update movement vectors
+        //without this check, gives error about zero vector
+        if(heading != Vector3.zero)
+        {
+            transform.forward = heading;
+            transform.position += rightMovement;
+            transform.position += upMovement;
+        }
+    }
+
+    //Speed double while holding Left Shift
+    void Sprint()
+    {
+        moveSpeed = 8f;
+        
+        Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey");
+        Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("VerticalKey");
+
+        Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+
+        //Debug.Log("SPRINT: heading: " + heading);
+        //Debug.Log("SPRINT: Right movement: " + rightMovement);
+        //Debug.Log("SPRINT: up movement: " + upMovement);
+
+        if (heading != Vector3.zero)
+        {
+            transform.forward = heading;
+            transform.position += rightMovement;
+            transform.position += upMovement;
+        }
+    }
+
+    //TODO
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickableObject"))
+        {
+            other.gameObject.SetActive(false);
+            //Destroy(this, 0.1f);
+        }
+        
+    }
+
+  
+
+    void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit floorHit;
+        Debug.Log("In Turning outside IF");
+
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+
+            playerToMouse.y = 0;
+
+            Debug.Log("In Turning inside IF... playerToMouse:" + playerToMouse);
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidbody.MoveRotation(newRotation);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Player/burnCalories.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class burnCalories : MonoBehaviour {
+
+    public float playerHealth = 1000.0f;
+    public float maxCalories = 2000.0f;
+    public float currentCalories;
+    public float calorieBurnRate;
+    //public bool isMoving = false;
+    //public bool isSedentary = false;
+    //public bool isActive = false;
+    //public bool isStrenuous = false;
+
+    public GameObject player;
+    
+
+	// Use this for initialization
+	void Start () {
+        currentCalories = maxCalories;
+
+        //temp 
+        //modCalorieBurn();
+
+        //continuously update calories
+        StartCoroutine("calorieConsumption");
+    }
+
+/*
+ * //TODO
+ *  
+    modCaloriesBurn - Checks current level of activity and switches calorieBurnRate based on what it finds 
+*/
+    public void modCalorieBurn()
+    {
+        if (Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.D)||Input.GetKey(KeyCode.S)||Input.GetKey(KeyCode.A))
+        {
+            calorieBurnRate = 2;
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                calorieBurnRate = 3;
+            }
+        }
+        
+        else
+        {
+            calorieBurnRate = 1;
+        }
+    }
+
+    /*Repeats every 1 second. 
+      As long as the player has calories left (currentCalories >0), the calorieBurnRate is subtracted from currentCalories once per second
+      If no calories left, calorieBurnRate is subtracted from health until health hits zero, then destroying player and setting to null to remove references
+    */
+    IEnumerator calorieConsumption()
+    {
+        while (true)
+        {
+            if (currentCalories != 0)
+            {
+                InvokeRepeating("calorieConsumption", 1.0f, 1.0f);
+                currentCalories = currentCalories - calorieBurnRate;
+                Debug.Log("Current Calories: " + currentCalories);
+            }
+            //if current calories at 0, subtract from health pool
+            if (currentCalories <= 0)
+            {
+                //if player has health, subtract calories burned from health
+                if (playerHealth > 0)
+                {
+                    InvokeRepeating("calorieConsumption", 1.0f, 1.0f);
+                    playerHealth = playerHealth - calorieBurnRate;
+                    Debug.Log("Current Calories: " + currentCalories);
+                    //player health is 0, kill player
+                    if (playerHealth <= 0)
+                    {
+                        playerHealth = 0;
+                        Debug.Log("You starved to death!");
+                        Destroy(player);
+                        player = null;
+                    }
+                }
+               
+              
+            }
+            yield return new WaitForSeconds(1.0f);
+        }
+       
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        modCalorieBurn();
+    }
+
+    
+
+}
